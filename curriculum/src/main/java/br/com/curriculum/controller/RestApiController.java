7d7350eb0614ae3306cb54abf2aef5b8d19e5a21
freit <freit@DESKTOP-KSVNOK1>
2017-05-16 16:07:14 -0300

    RestController em desenvolvimento.

--- curriculum/src/main/java/br/com/curriculum/controller/RestApiController.java
+++ curriculum/src/main/java/br/com/curriculum/controller/RestApiController.java
@@ -1,9 +1,15 @@
 package br.com.curriculum.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.curriculum.domain.Usuario;
 import br.com.curriculum.services.UsuarioService;
 
 @RestController
@@ -13,5 +19,12 @@ public class RestApiController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
-	
+	@RequestMapping(value = "/users/", method = RequestMethod.GET)
+	public ResponseEntity<List<Usuario>> listAllUsers() {
+		List<Usuario> users = usuarioService.getUsers();
+		if (users.isEmpty())
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		else
+			return new ResponseEntity<List<Usuario>>(users, HttpStatus.OK);
+	}
 }
